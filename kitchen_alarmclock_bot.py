6b809cdad5250f4f131603de380ab14a161de3ae
svmatveev <54777529+svmatveev@users.noreply.github.com>
2022-03-28 17:20:08 +0300

    Update kitchen_alarmclock_bot.py

--- kitchen_alarmclock_bot.py
+++ kitchen_alarmclock_bot.py
@@ -1,5 +1,5 @@
-from tokenize import String
 import ptbot
+import random
 
 
 
@@ -7,10 +7,23 @@ def read_secrets():
     with open("./secrets/telegram_token.txt", "r") as file:
         return file.readlines()
 
+
+def choose():
+    answes = ("да", "нет", "это возможно")
+    choice = random.choice(answes)
+    message = f"Думаю, {choice}"
+    bot.send_message(TG_CHAT_ID, message)
+    
 if __name__ == "__main__":
+    
     secrets_list = read_secrets()
     TG_TOKEN = secrets_list[0].rstrip()
     TG_CHAT_ID = secrets_list[1].rstrip()
-    print(TG_TOKEN, TG_CHAT_ID, sep="\n")
+
     bot = ptbot.Bot(TG_TOKEN)
-    bot.send_message(TG_CHAT_ID, "Бот запущен")
+    choose()
+    choose()
+    
+
+
+
