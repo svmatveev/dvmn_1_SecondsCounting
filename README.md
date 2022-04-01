0584f6199d06631bac3c4a8cb2aa0c84fbd0779f
svmatveev <54777529+svmatveev@users.noreply.github.com>
2022-04-01 16:33:21 +0300

    task finished
    First try to pass the validation

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# dvmn_1_SecondsCounting
- Telegram bot creation. First steps
+# Seconds timer
+
+[Devman](https://dvmn.org/modules/meeting-python/lesson/timer-in-telegram) Task 5 implementation of Python introduction module.
+
+This is a simple cooking-helper telegram bot.
+Supporting features:
+- Cooking timer
+
+
+**Prerequisites**
+---
+Telegram bot should be registered using @BotFather. Api-token should be found.
+
+Telegram bot api-roken should be stored at "secrets" folder inside "telegram_token.txt" file. So, the path to api key should be resolved by "./secrets/telegram_token.txt" path from "kitchen_alarmclock_bot.py" script file.
+
+Python3 should be installed
+
+Additional modules required:
+- pytimeparse==1.1.8
+- telegram==0.0.1
+- [ptbot.py](https://gist.github.com/dvmn-tasks/e603319227656c63e486831bf4673f26) Should be inside the folder where "kitchen_alarmclock_bot.py" is placed. ([ptbot.py docs](https://dvmn.org/encyclopedia/modules/ptbot_docs/))
+
+Dependencies can be solved by running:
+```
+pip install -r requirements.txt
+``` 
+
+**Usage**
+---
+1. You can join newly-created bot using Telegram
+2. Run kitchen_alarmclock_bot.py from command line:
+```
+$ python3 kitchen_alarmclock_bot.py
+```
+3. Send */start* command
+4. Now you can send the time you want to countdown
+
+**Authors**
+---
+[Sergey M](mailto:svmatveev1988@yandex.ru)
+
+**License**
+---
+This project is licensed under the MIT License - see the LICENSE.md file for details
--- kitchen_alarmclock_bot.py
+++ kitchen_alarmclock_bot.py
@@ -1,6 +1,6 @@
-import ptbot
-import random
+import pytimeparse
 
+import ptbot
 
 
 def read_secrets():
@@ -8,22 +8,36 @@ def read_secrets():
         return file.readlines()
 
 
-def choose():
-    answes = ("да", "нет", "это возможно")
-    choice = random.choice(answes)
-    message = f"Думаю, {choice}"
-    bot.send_message(TG_CHAT_ID, message)
-    
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix) 
+
+
+def notify_progress(secs_left, chat_id, message_id, total_delay):
+    progress_bar = render_progressbar(total_delay, total_delay-secs_left)
+    bot.update_message(chat_id, message_id, f"Осталось {secs_left} секунд\n{progress_bar}")
+    if secs_left == 0:
+        bot.send_message(chat_id, "Время вышло")
+
+
+def execute_after_secs_specified(chat_id, message):
+    delay = pytimeparse.parse(message)
+    if delay is None:
+        message = "Некорректный формат времени задержки таймера"
+        bot.send_message(chat_id, message)
+    else:
+        message_id = bot.send_message(chat_id, "Запуск таймера")
+        bot.create_countdown(delay, notify_progress, chat_id=chat_id, message_id=message_id, total_delay=delay)
+
+
 if __name__ == "__main__":
-    
     secrets_list = read_secrets()
     TG_TOKEN = secrets_list[0].rstrip()
-    TG_CHAT_ID = secrets_list[1].rstrip()
 
     bot = ptbot.Bot(TG_TOKEN)
-    choose()
-    choose()
-    
-
-
-
+    bot.reply_on_message(execute_after_secs_specified)
+    bot.run_bot()
