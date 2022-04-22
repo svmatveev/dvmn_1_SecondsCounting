88f8f6a0cf8fc484c2c4edb00dd60ab5739a2624
svmatveev <54777529+svmatveev@users.noreply.github.com>
2022-04-22 11:21:10 +0300

    refactoring

--- kitchen_bot.py
+++ kitchen_bot.py
@@ -3,7 +3,29 @@ import pytimeparse
 import ptbot
 
 
-def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+def execute_with_period_set_in_mesage(chat_id, message, bot):
+    delay = pytimeparse.parse(message)
+    if delay is None:
+        message = "Некорректный формат времени задержки таймера"
+        bot.send_message(chat_id, message)
+    else:
+        message_id = bot.send_message(chat_id, "Запуск таймера")
+        bot.create_countdown(delay,
+                             notify_progress,
+                             chat_id=chat_id,
+                             message_id=message_id,
+                             total_delay=delay,
+                             bot=bot)
+
+
+def render_progressbar(total,
+                       iteration,
+                       prefix='',
+                       suffix='',
+                       length=30,
+                       fill='█',
+                       zfill='░'):
+
     iteration = min(total, iteration)
     percent = "{0:.1f}"
     percent = percent.format(100 * (iteration / float(total)))
@@ -12,34 +34,24 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix) 
 
 
-def notify_progress(secs_left, chat_id, message_id, total_delay):
+def notify_progress(secs_left, chat_id, message_id, total_delay, bot):
     progress_bar = render_progressbar(total_delay, total_delay-secs_left)
-    bot.update_message(chat_id, message_id, f"Осталось {secs_left} секунд\n{progress_bar}")
+    bot.update_message(chat_id, message_id,
+                       f"Осталось {secs_left} секунд\n{progress_bar}")
     if secs_left == 0:
         bot.send_message(chat_id, "Время вышло")
 
 
-def execute_with_period_set_in_mesage(chat_id, message):
-    delay = pytimeparse.parse(message)
-    if delay is None:
-        message = "Некорректный формат времени задержки таймера"
-        bot.send_message(chat_id, message)
-    else:
-        message_id = bot.send_message(chat_id, "Запуск таймера")
-        bot.create_countdown(delay, notify_progress, chat_id=chat_id, message_id=message_id, total_delay=delay)
-
-
 def main():
-    bot.reply_on_message(execute_with_period_set_in_mesage)
+    with open("./secrets/telegram_token.txt", "r") as file:
+        secrets_list = file.readlines()
+        telegram_token = secrets_list[0].rstrip()
+
+        bot = ptbot.Bot(telegram_token)
+
+    bot.reply_on_message(execute_with_period_set_in_mesage, bot=bot)
     bot.run_bot()
 
 
 if __name__ == "__main__":
-
-    with open("./secrets/telegram_token.txt", "r") as file:
-        secrets_list =  file.readlines()
-        telegram_token = secrets_list[0].rstrip()
-
-    bot = ptbot.Bot(telegram_token)
-
     main()
